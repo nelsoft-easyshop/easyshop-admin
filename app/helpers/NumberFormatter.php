2f36dd65030dc15541a31593e2686e1e07e034a7
Sam <samgavinio@easyshop.ph>
2014-07-31 11:30:49 +0800

    #18 Added space around concatanation operator

--- app/helpers/NumberFormatter.php
+++ app/helpers/NumberFormatter.php
@@ -17,9 +17,9 @@ class NumberFormatter{
         if (!in_array(($num % 100),array(11,12,13))){
             switch ($num % 10) {
                 // Handle 1st, 2nd, 3rd
-                case 1:  return $num.'st';
-                case 2:  return $num.'nd';
-                case 3:  return $num.'rd';
+                case 1:  return $num . 'st';
+                case 2:  return $num . 'nd';
+                case 3:  return $num . 'rd';
             }
         }
         return $num.'th';
