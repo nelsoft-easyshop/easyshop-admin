5eaccc29a7f04e56ba6e9f9f923d5948d515475e
Sam <samgavinio@easyshop.ph>
2014-08-02 05:18:18 +0800

    Added GET and PUT methods for payment accounts

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -20,7 +20,7 @@ class OrderController extends BaseController
    /**
     *  GET method for displaying specific order products in a payment account
     *
-    *  @return View
+    *  @return JSON
     */
     public function getOrderProducts()
     {
@@ -36,7 +36,7 @@ class OrderController extends BaseController
    /**
     *  GET method for displaying order product history
     *
-    *  @return View
+    *  @return JSON
     */
     public function getOrderProductDetail()
     {
@@ -49,15 +49,53 @@ class OrderController extends BaseController
                     ->render();
         return Response::json(array('html' => $html));
     }
-    
+   
+   /**
+    *  GET method for displaying payment details for an order product
+    *
+    *  @return JSON
+    */
     public function getOrderProductPaymentDetail(){
         $userdata = Input::get();
         $orderProductRepository = App::make('OrderProductRepository');
+        $billingInfoRepository = App::make('BillingInfoRepository');
+        $bankInfoRepository = App::make('BankInfoRepository');
         
+        $orderProduct = $orderProductRepository->getOrderProductById($userdata['order_product_id']);
+        $paymentAccounts = $billingInfoRepository->getBillingAccountsByMemberId($orderProduct->seller_id);
+        $bankList = $bankInfoRepository->getAllBanks();
+
         $html = View::make('partials.orderproductbilling')
-                    ->with('accounts', array())
+                    ->with('accounts', $paymentAccounts)
+                    ->with('defaultAccount', $orderProduct->billingInfo)
+                    ->with('bankList', $bankList)
                     ->render();
         return Response::json(array('html' => $html));
+    }
+    
+   /**
+    * PUT method for updating payment account
+    *
+    * @return JSON
+    */
+    public function updateOrderProductPaymentAccount(){
+        $rules = array(
+            'billing_info_id' => 'required|numeric',
+            'account_name' => 'required',
+            'account_no' => 'required',
+            'bank_id' => 'required|numeric'
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            $isSuccessful = false;
+            $message = $validator->messages();
+        }else{
+            $userdata = Input::get();
+            $billingInfoRepository = App::make('BillingInfoRepository');
+            $billingInfoRepository->saveBillingAccount($userdata['billing_info_id'], $userdata['account_name'], $userdata['account_number'], $userdata['bank_id']);
+        }
+        
     }
     
     
