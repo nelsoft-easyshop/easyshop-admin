045de9823896dd39619cb94b320043dd13ce9b1a
Sam <samgavinio@easyshop.ph>
2014-08-03 20:49:32 +0800

    #18 Replaced tabs with spaces

--- app/libraries/Easyshop/Composers/ComposerServiceProvider.php
+++ app/libraries/Easyshop/Composers/ComposerServiceProvider.php
@@ -4,17 +4,17 @@ use Illuminate\Support\ServiceProvider;
 
 class ComposerServiceProvider extends ServiceProvider 
 {
-	
-	/**
-	* Register a view composer to a particular view
-	*
-	*/
- 
-	public function register()
-	{
-		$this->app->view->composer('includes.header', 'Easyshop\Composers\HeaderComposer');
-		$this->app->view->composer('pages.paymentlist', 'Easyshop\Composers\PaymentComposer');
-		$this->app->view->composer('partials.orderproductbilling', 'Easyshop\Composers\BillingDetailComposer');
-	}
+
+   /**
+    * Register a view composer to a particular view
+    *
+    */
+
+    public function register()
+    {
+        $this->app->view->composer('includes.header', 'Easyshop\Composers\HeaderComposer');
+        $this->app->view->composer('pages.paymentlist', 'Easyshop\Composers\PaymentComposer');
+        $this->app->view->composer('partials.orderproductbilling', 'Easyshop\Composers\BillingDetailComposer');
+    }
  
 }
