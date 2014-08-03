056682e37cddb3658ffd5181cadf8c593081e4ee
Sam <samgavinio@easyshop.ph>
2014-08-03 20:45:31 +0800

    #18 Change tabs to spaces.

--- app/database/migrations/2014_08_01_123914_create_shipping_comment_table.php
+++ app/database/migrations/2014_08_01_123914_create_shipping_comment_table.php
@@ -5,35 +5,35 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateShippingCommentTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('es_product_shipping_comment', function(Blueprint $table)
-		{
-			$table->increments('id_shipping_comment');
-			$table->integer('order_product_id');
-			$table->string('courier', 255);
-			$table->string('tracking_num', 255);
-			$table->text('comment');
-			$table->integer('member_id');
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::create('es_product_shipping_comment', function(Blueprint $table)
+        {
+            $table->increments('id_shipping_comment');
+            $table->integer('order_product_id');
+            $table->string('courier', 255);
+            $table->string('tracking_num', 255);
+            $table->text('comment');
+            $table->integer('member_id');
             $table->dateTime('expected_date');
-			$table->dateTime('delivery_date');
-			$table->timestamps('');
-		});
-	}
+            $table->dateTime('delivery_date');
+            $table->timestamps('');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('es_product_shipping_comment');
-	}
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::drop('es_product_shipping_comment');
+    }
 
 }
--- app/database/migrations/2014_08_02_012829_create_bank_table.php
+++ app/database/migrations/2014_08_02_012829_create_bank_table.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateBankTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('es_bank_info', function(Blueprint $table)
-		{
-			$table->increments('id_bank');
-			$table->string('bank_name', 255);
-			$table->string('bank_short_name', 255);
-			$table->timestamps();
-		});
-	}
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::create('es_bank_info', function(Blueprint $table)
+        {
+            $table->increments('id_bank');
+            $table->string('bank_name', 255);
+            $table->string('bank_short_name', 255);
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('es_bank_info');
-	}
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::drop('es_bank_info');
+    }
 
 }
