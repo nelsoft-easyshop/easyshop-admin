e901a7860fe9fa1f30a826afa7456edc80adf3f5
Sam <samgavinio@easyshop.ph>
2014-08-07 12:49:35 +0800

    Created emailService. Currently contains method to send email to paid users

---
+++ app/libraries/Easyshop/Services/EmailService.php
@@ -0,0 +1,35 @@
+<?php namespace Easyshop\Services;
+
+use Illuminate\Support\Facades\Mail;
+
+class EmailService{
+    
+   /**
+    * Send email for sellers that have been paid
+    *
+    * @param Entity $member
+    * @param Entity[] $orderProducts
+    * @param Entity $account
+    * @param Carbon $dateFrom
+    * @param Carbon $dateTo
+    *
+    */
+    public function sendPaymentNotice($member, $orderProducts, $account, $dateFrom, $dateTo)
+    {
+        $data = array(
+            'recipient' => $member->username,
+            'startPayOutDate' => $dateFrom,
+            'endPayOutDate' => $dateTo,
+            'orderProducts' => $orderProducts,
+            'account' => $account,
+        );
+
+        Mail::send('emails.sellerpaid', $data, function($message) use ($member)
+        {
+            $message->to($member->email, isset($member->fullname) ? $member->fullname : $member->username )
+                ->subject('Easyshop.ph - Payment for your sales.');
+        });
+    }
+    
+
+}
