50ec3996e78427e570f4c36d6fc9d708252b738a
Sam <samgavinio@easyshop.ph>
2014-08-07 12:47:21 +0800

    Added OrderStatusRepository. Currently only contains methods to get status from DB

---
+++ app/libraries/Easyshop/ModelRepositories/OrderStatusRepository.php
@@ -0,0 +1,25 @@
+<?php namespace Easyshop\ModelRepositories;
+
+use Illuminate\Support\Facades\DB;
+use OrderStatus;
+
+class OrderStatusRepository
+{    
+    public function getPaidStatus()
+    {
+        return OrderStatus::find(OrderStatus::STATUS_PAID);
+    }
+    
+    public function getCompletedStatus()
+    {
+        return OrderStatus::find(OrderStatus::STATUS_COMPLETED);
+    }
+    
+    public function getDraftStatus()
+    {
+        return OrderStatus::find(OrderStatus::STATUS_DRAFT);
+    }
+
+}
+
+
