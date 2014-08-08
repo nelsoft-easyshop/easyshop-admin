8f1a35f514c32ad8f64d557bf35b306ca615949e
Sam <samgavinio@easyshop.ph>
2014-08-08 18:10:44 +0800

    Added OrderProductBillingInfo update validator

---
+++ app/libraries/Easyshop/Services/Validation/Laravel/OrderProductBillingInfoUpdateValidator.php
@@ -0,0 +1,21 @@
+<?php namespace Easyshop\Services\Validation\Laravel;
+ 
+use Easyshop\Services\Validation\ValidatorInterface;
+ 
+class OrderProductBillingInfoUpdateValidator extends LaravelValidator implements ValidatorInterface {
+ 
+   /**
+    * Validation for updating OrderProductBillingInfo
+    *
+    * @var array
+    */
+    protected $rules = array(
+        'order_product_billing_info_id' => 'required|numeric',
+        'account_name' => 'required',
+        'account_number' => 'required',
+        'bank_name' => 'required',
+    );
+
+ 
+}
+
