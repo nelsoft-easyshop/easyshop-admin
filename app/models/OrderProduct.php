198c96a7112d4afceec60afe8b614bcd2ea8e2d8
Sam <samgavinio@easyshop.ph>
2014-08-10 02:40:28 +0800

    Added docblocks and replaced orderProductBillingInfo reference to orderBillingInfo

--- app/models/OrderProduct.php
+++ app/models/OrderProduct.php
@@ -3,49 +3,78 @@
 class OrderProduct extends Eloquent 
 {
     
-   /**
-    * The database table used by the model.
-    *
-    * @var string
-    */
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table = 'es_order_product';
 
-   /**
-    * The primary key of the table
-    *
-    */
+    /**
+     * The primary key of the table
+     *
+     */
     protected $primaryKey = 'id_order_product';
 
+    
+    /**
+     * Relationship with es_member table
+     *
+     */
     public function seller() 
     {
         return $this->hasOne('Member', 'id_member', 'seller_id');
     }
 
+    /**
+     * Relationship with es_order
+     *
+     */
     public function order() 
     {
         return $this->hasOne('Order', 'id_order', 'order_id');
     }
 
+    /**
+     * Relationship with es_order_product_status
+     *
+     */
     public function orderProductStatus() 
     {
         return $this->hasOne('OrderProductStatus', 'id_order_product_status', 'status');
     }
 
+    /**
+     *  Relationship with es_order_product_status
+     *
+     */
     public function product() 
     {
         return $this->hasOne('Product', 'id_product', 'product_id');
     }
     
+    /**
+     * Relationship with es_order_billing_info
+     *
+     */
     public function sellerBillingInfo() 
     {
-        return $this->hasOne('OrderProductBillingInfo', 'id_order_billing_info', 'seller_billing_id');
+        return $this->hasOne('OrderBillingInfo', 'id_order_billing_info', 'seller_billing_id');
     }
     
+    /**
+     * Relationship with es_order_product_history
+     *
+     */
     public function orderProductHistory()
     {
         return $this->hasMany('OrderProductHistory', 'order_product_id', 'id_order_product');
     }
     
+    /**
+     * Relationship with es_order_shipping_comment
+     *
+     */
     public function orderProductComment()
     {
         return $this->hasOne('ProductShippingComment', 'order_product_id', 'id_order_product');
