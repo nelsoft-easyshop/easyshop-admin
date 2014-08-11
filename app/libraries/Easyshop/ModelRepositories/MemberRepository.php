36a4a595f1867645d73fcf1ccdcbed00d07381d9
Sam <samgavinio@easyshop.ph>
2014-08-11 12:24:51 +0800

    Added nested where to getUsersToPay to fix bug where the username is not used properly when filtering

--- app/libraries/Easyshop/ModelRepositories/MemberRepository.php
+++ app/libraries/Easyshop/ModelRepositories/MemberRepository.php
@@ -55,24 +55,26 @@ class MemberRepository extends BaseRepository
         });
         
         $query->leftJoin('es_product_shipping_comment','es_product_shipping_comment.order_product_id', '=', 'es_order_product.id_order_product');
+        
         $query->where(function ($query) use ($dateFrom, $dateTo){
-            $query->where('es_order_product_history.created_at', '>=', $dateFrom);
-            $query->where('es_order_product_history.created_at', '<', $dateTo);
-        });
+            $query->where(function ($query) use ($dateFrom, $dateTo){
+                $query->where('es_order_product_history.created_at', '>=', $dateFrom);
+                $query->where('es_order_product_history.created_at', '<', $dateTo);
+            });
 
-        $query->orWhere(function ($query) use ($dateFrom, $dateTo) {
-            $query->where(function ($query) {
-                            $query->where('es_order.order_status', '=', OrderStatus::STATUS_PAID)
-                                ->orWhere('es_order.order_status', '=',  OrderStatus::STATUS_COMPLETED);
-                        });
-            $query->where('es_order_product.status', '=', OrderProductStatus::STATUS_ON_GOING);
-            $query->where('es_order_product.is_reject', '=', '0');
-            $query->whereNotNull('es_product_shipping_comment.id_shipping_comment');
-            $query->where(DB::raw("DATEDIFF(?,es_product_shipping_comment.delivery_date) >= 15"));
-            $query->setBindings(array_merge($query->getBindings(),array($dateTo)));
-            $query->where(DB::raw(" DATE_ADD(es_product_shipping_comment.`delivery_date`, INTERVAL 15 DAY) BETWEEN ? AND ?"));
-            $query->setBindings(array_merge($query->getBindings(),array($dateFrom, $dateTo)));
-
+            $query->orWhere(function ($query) use ($dateFrom, $dateTo) {
+                $query->where(function ($query) {
+                                $query->where('es_order.order_status', '=', OrderStatus::STATUS_PAID)
+                                    ->orWhere('es_order.order_status', '=',  OrderStatus::STATUS_COMPLETED);
+                            });
+                $query->where('es_order_product.status', '=', OrderProductStatus::STATUS_ON_GOING);
+                $query->where('es_order_product.is_reject', '=', '0');
+                $query->whereNotNull('es_product_shipping_comment.id_shipping_comment');
+                $query->where(DB::raw("DATEDIFF(?,es_product_shipping_comment.delivery_date) >= 15"));
+                $query->setBindings(array_merge($query->getBindings(),array($dateTo)));
+                $query->where(DB::raw(" DATE_ADD(es_product_shipping_comment.`delivery_date`, INTERVAL 15 DAY) BETWEEN ? AND ?"));
+                $query->setBindings(array_merge($query->getBindings(),array($dateFrom, $dateTo)));
+            });
         });
         
         if(!empty($username)){
@@ -84,6 +86,40 @@ class MemberRepository extends BaseRepository
 
         
         return $completedOrders;
+    }
+
+    /**
+     * Get users to be refunded. Results are grouped by user.
+     *
+     * 
+     */
+    public function getUserAccountsToRefund($username, $dateFrom, $dateTo)
+    {
+        $dateFrom = $dateFrom->format('Y-m-d');
+        $dateTo = $dateTo->format('Y-m-d');
+        
+        $query = DB::table('es_order_product')->join('es_member','es_order_product.seller_id', '=', 'es_member.id_member');
+        $query->join('es_order','es_order_product.order_id', '=', 'es_order.id_order');
+        
+        $query->join('es_order_product_history', function($join){
+            $join->on('es_order_product.id_order_product', '=', 'es_order_product_history.order_product_id');
+            $join->on('es_order_product_history.order_product_status', '=',  DB::raw(OrderProductStatus::STATUS_RETURN_BUYER));
+        });
+        $query->where(function ($query) use ($dateFrom, $dateTo){
+            $query->where('es_order_product_history.created_at', '>=', $dateFrom);
+            $query->where('es_order_product_history.created_at', '<', $dateTo);
+        });
+        
+        if(!empty($username)){
+            $query->where('es_member.username', '=', $username);
+        }     
+        
+        $query->groupBy('es_member.id_member');
+        
+        $returnedOrders = $query->get(['es_member.username', 'es_member.email', 'es_member.contactno', DB::raw('SUM(es_order_product.net) as net')]);
+
+        return $returnedOrders;
+    
     }
 }
 
