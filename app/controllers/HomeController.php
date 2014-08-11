cc91878037a682df219f2e55f77e1ed51ffe53d0
kurtwilkinson024 <kurtwilkinson024@easyshop.ph>
2014-08-11 16:57:23 +0800

    Create MemberController,change routes

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,34 +6,4 @@ class HomeController extends BaseController
         return View::make('pages.dashboard')->with('username', Auth::user()->username);
     }
 
-    public function showAllUsers()
-    {
-        $listOfLoc = App::make('LocationLookUpRepository');
-        $dataFormatter = App::make('Easyshop\Services\DataFormatterService');
-        $data = $dataFormatter->location($listOfLoc->getLocationByType());
-
-        return View::make('pages.userlist')->with('list_of_member', Member::paginate(100))->with('list_of_location', $data);
-    }
-
-    public function ajaxUpdateUsers()
-    {
-        $dataMember = array(
-            'fullname' => Input::get('fullname'),
-            'contactno' => Input::get('contact'),
-            'remarks' => Input::get('remarks'),
-            'is_promo_valid' => Input::get('is_promo_valid')
-        );
-        $dataAddress = array(
-            'city' => Input::get('city'),
-            'stateregion' => Input::get('stateregion'),
-            'address' => Input::get('address'),
-            'country' => 148
-        );
-        $memberRepository = App::make('MemberRepository');
-        $memberRepository->update(Input::get('id'), $dataMember);
-        $addressRepository = App::make('AddressRepository');
-        $addressRepository->update(Input::get('id'), $dataAddress);
-
-        echo json_encode($memberRepository->getById(Input::get('id')));
-    }
 }
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -29,12 +29,6 @@
 		</div>
 
 	</body>
-
-    @yield('javascript')
-
-        </div>
-        
-    </body>
     
     {{ HTML::script('js/src/jquery-2.0.0.min.js') }}
     {{ HTML::script('js/src/bootstrap.min.js') }}
