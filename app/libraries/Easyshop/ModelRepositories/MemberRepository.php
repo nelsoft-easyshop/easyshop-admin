1265c4935a6337708971b720dba1e9cf228adc5d
Sam <samgavinio@easyshop.ph>
2014-08-12 17:43:01 +0800

    #36 Added H:i:s to carbon date-format.Also added missing join with es_billing_info for getUserAccountsToRefund

--- app/libraries/Easyshop/ModelRepositories/MemberRepository.php
+++ app/libraries/Easyshop/ModelRepositories/MemberRepository.php
@@ -43,8 +43,8 @@ class MemberRepository extends BaseRepository
     public function getUserAccountsToPay($username, $dateFrom, $dateTo)
     {
 
-        $dateFrom = $dateFrom->format('Y-m-d');
-        $dateTo = $dateTo->format('Y-m-d');
+        $dateFrom = $dateFrom->format('Y-m-d H:i:s');
+        $dateTo = $dateTo->format('Y-m-d H:i:s');
         
         $query = DB::table('es_order_product')->leftJoin('es_order_billing_info', 'es_order_product.seller_billing_id', '=', 'es_order_billing_info.id_order_billing_info');
         $query->join('es_member','es_order_product.seller_id', '=', 'es_member.id_member');
@@ -95,11 +95,17 @@ class MemberRepository extends BaseRepository
      */
     public function getUserAccountsToRefund($username, $dateFrom, $dateTo)
     {
-        $dateFrom = $dateFrom->format('Y-m-d');
-        $dateTo = $dateTo->format('Y-m-d');
+        $dateFrom = $dateFrom->format('Y-m-d H:i:s');
+        $dateTo = $dateTo->format('Y-m-d H:i:s');
         
-        $query = DB::table('es_order_product')->join('es_member','es_order_product.seller_id', '=', 'es_member.id_member');
-        $query->join('es_order','es_order_product.order_id', '=', 'es_order.id_order');
+        $query = DB::table('es_order_product')->join('es_order','es_order_product.order_id', '=', 'es_order.id_order');
+        $query->join('es_member','es_order.buyer_id', '=', 'es_member.id_member');
+        $query->leftJoin('es_billing_info',function($leftJoin){
+            $leftJoin->on('es_billing_info.member_id', '=', 'es_member.id_member');
+            $leftJoin->on('es_billing_info.is_default', '=',  DB::raw('1'));
+        });
+        
+        $query->leftJoin('es_bank_info', 'es_billing_info.bank_id', '=', 'es_bank_info.id_bank');
         
         $query->join('es_order_product_history', function($join){
             $join->on('es_order_product.id_order_product', '=', 'es_order_product_history.order_product_id');
@@ -109,15 +115,15 @@ class MemberRepository extends BaseRepository
             $query->where('es_order_product_history.created_at', '>=', $dateFrom);
             $query->where('es_order_product_history.created_at', '<', $dateTo);
         });
-        
+
         if(!empty($username)){
             $query->where('es_member.username', '=', $username);
         }     
         
         $query->groupBy('es_member.id_member');
         
-        $returnedOrders = $query->get(['es_member.username', 'es_member.email', 'es_member.contactno', DB::raw('SUM(es_order_product.net) as net')]);
-
+        $returnedOrders = $query->get(['es_member.username', 'es_member.email', 'es_member.contactno', 'es_billing_info.bank_account_name as account_name', 'es_billing_info.bank_account_number as account_number', 'es_bank_info.bank_name as bank_name' , DB::raw('SUM(es_order_product.net) as net')]);
+        
         return $returnedOrders;
     
     }
