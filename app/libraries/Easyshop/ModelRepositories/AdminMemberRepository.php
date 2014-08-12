fa38936664c365f41e2cc22a928afebb0e3c57b7
unknown <inon.baguio@yahoo.com>
2014-08-12 15:13:13 +0800

    1. Renamed the filename from AdminRepository to AdminMemberRepository 2. Renamed the getAdmin method to getAdminMemberById

---
+++ app/libraries/Easyshop/ModelRepositories/AdminMemberRepository.php
@@ -0,0 +1,20 @@
+<?php namespace Easyshop\ModelRepositories;
+
+use AdminMember;
+
+class AdminMemberRepository
+{
+    /**
+    * Get order by id
+    *
+    * @param string $slug
+    * @return Entity
+    */
+
+    public function getAdminMemberById($id)
+    {
+        return AdminMember::find($id)->pluck('password');
+    }
+    
+}
+
