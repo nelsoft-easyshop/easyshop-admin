f0f667f9359695afd7db67f7fd6d5a0c14127f4d
Sam <samgavinio@easyshop.ph>
2014-08-12 17:35:32 +0800

    #36 Replaced one of the get data from seller_id to member_id

--- app/controllers/BillingInfoController.php
+++ app/controllers/BillingInfoController.php
@@ -34,7 +34,7 @@ class BillingInfoController extends BaseController
         $billingInfoRepository->createBillingAccount(Input::get('account_name'),
                                         Input::get('account_number'),
                                         Input::get('bank_id'), 
-                                        Input::get('seller_id'));
+                                        Input::get('member_id'));
             
         return Response::json(array('errors' => $billingInfoRepository->getErrors(), 'new_billing_info_id' => $billingInfoRepository->getLastId()));
     }
