88b5cc6703463508dcff6df8dcd218181c08e2d4
unknown <inon.baguio@yahoo.com>
2014-08-15 16:50:59 +0800

    #24 Added the usage of Dependecy Injection Service. New Service added: XMLContentGetterService

--- app/controllers/ContentManagerController.php
+++ app/controllers/ContentManagerController.php
@@ -1,7 +1,16 @@
 <?php
-  
+use Easyshop\Services\XMLContentGetterService as XMLService;
+
+
+
 class ContentManagerController extends BaseController 
 {
+    protected $XMLService;
+
+    public function __construct(XMLService $XMLService)
+    {
+        $this->XMLService = $XMLService;
+    }
     
     public function getHomeContent()
     {
@@ -17,7 +26,6 @@ class ContentManagerController extends BaseController
 
             $productSlides[] =  $productEntity->getProductBySlug($slides->value);
             $productTypes[] = $slides;
-            $obj = json_encode(array_merge($productSlides,$productTypes));
         }
         foreach($map->section as $section)
         {
@@ -31,7 +39,14 @@ class ContentManagerController extends BaseController
         {
             $nodeTypes[] =  $types;
         }
-        
+
+        $productSlidesEncode = json_encode($productSlides);
+        $productTypesEncode = json_encode($productTypes);
+
+        $productSlidesDecode = json_decode($productSlidesEncode, TRUE);
+        $productTypesDecode = json_decode($productTypesEncode, TRUE);
+
+
         return View::make('pages.cms-home')
             ->with('adminPassword', $adminEntity->getAdminMemberById($id))
             ->with('userId', $id)
@@ -42,13 +57,14 @@ class ContentManagerController extends BaseController
             ->with('mainSlides',  $mainSlides)
             ->with('mainSlideId',  0)
             ->with('mainSlideCount',  count($mainSlides))
-            ->with('productSlide',  json_encode($productSlides))
+            ->with('productSlide',  $productSlidesDecode)
             ->with('slugs',  $productSlides)
             ->with('productSlideCount',  count($productTypes))
-            ->with('productTypes',  json_encode($productTypes))
+            ->with('productTypes',  $productTypesDecode)
             ->with('productSlideId', 0 )
-            ->with('sectionId', 0 )
+            ->with('sectionId', -1 )
             ->with('panelMainId', 0 )
+            ->with('collapse', 0 )
             ->with('nodeTypes', $nodeTypes);
            
     }
@@ -56,14 +72,16 @@ class ContentManagerController extends BaseController
     public function getMainSlides()
     {
         $id = Auth::id();
-        $xmlString = file_get_contents("https://easyshop.ph.feature/webservice/homewebservice/getContents/");
+        $adminEntity = App::make('AdminMemberRepository');
+        $xmlString = $this->XMLService->GetXmlContent();
         $map = simplexml_load_string(trim($xmlString));
         foreach($map->mainSlide as $slides)
         {
             $mainSlides[] =  $slides;
         }
 
-        return View::make('includes.mainslides')
+        return View::make('partials.mainslides')
+            ->with('adminPassword', $adminEntity->getAdminMemberById($id))
             ->with('userId', $id)
             ->with('mainSlides',$mainSlides)
             ->with('mainSlideId',0)
@@ -73,18 +91,20 @@ class ContentManagerController extends BaseController
     public function getProductSlides()
     {
         $id = Auth::id();
-        $xmlString = file_get_contents("https://easyshop.ph.feature/webservice/homewebservice/getContents/");
+        $adminEntity = App::make('AdminMemberRepository');
+        $xmlString = $this->XMLService->GetXmlContent();
         $map = simplexml_load_string(trim($xmlString));
+
         $productEntity = App::make('ProductRepository');
         foreach($map->productSlide as $slides)
         {
 
             $productSlides[] =  $productEntity->getProductBySlug($slides->value);
             $productTypes[] = $slides;
-            $obj = json_encode(array_merge($productSlides,$productTypes));
         }
 
-        return View::make('includes.productslides')
+        return View::make('partials.productslides')
+            ->with('adminPassword', $adminEntity->getAdminMemberById($id))
             ->with('userId', $id)
             ->with('productSlide',  json_encode($productSlides))
             ->with('slugs',  $productSlides)
