3cba2978807202e9990cfb7026afde467a6cf6d8
unknown <inon.baguio@yahoo.com>
2014-08-18 19:12:22 +0800

    #24 Modified the docblock

--- app/libraries/Easyshop/Services/XMLContentGetterService.php
+++ app/libraries/Easyshop/Services/XMLContentGetterService.php
@@ -2,17 +2,15 @@
 
 class XMLContentGetterService
 {
-
+    /**
+     * Returns xml content from of home_files.xml from easyshop.ph
+     * 
+     * @return $xmlString
+     */
     public function GetXmlContent()
     {
         $xmlString = file_get_contents("https://easyshop.ph.feature/webservice/homewebservice/getContents/");
         return $xmlString;
     }
 
-    /**
-     * Returns xml content from of home_files.xml from easyshop.ph
-     *
-     * 
-     * @return $xmlString
-     */
 }
