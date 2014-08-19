1eadae7e0dab125e00c090e6a2129af6e78a9661
Sam <samgavinio@easyshop.ph>
2014-08-19 15:15:44 +0800

    hasDefaultAccount is now getDefaultAccount

--- app/libraries/Easyshop/ModelRepositories/BillingInfoRepository.php
+++ app/libraries/Easyshop/ModelRepositories/BillingInfoRepository.php
@@ -73,7 +73,14 @@ class BillingInfoRepository extends AbstractRepository
         $billingInfo->bank_account_number = $accountNumber;
         $billingInfo->member_id = $memberId;
         $billingInfo->bank_id = $bankId;
-        $billingInfo->is_default = $this->hasDefaultAccount($memberId) ? '0' : '1';
+        
+        if($this->getDefaultAccount($memberId)->isEmpty()){
+            billingInfo->is_default = 1;
+        }
+        else{
+            billingInfo->is_default = 0;
+        }
+        
         $isSuccessful = $billingInfo->save();
         
         $this->currentId = $billingInfo->id_billing_info;
@@ -84,16 +91,17 @@ class BillingInfoRepository extends AbstractRepository
     
     
     /**
-     * Return whether a default account exists for a particular member
+     * Return the default account for a particular member
      *
      * @param integer $userId
-     * @return boolean
+     * @return BillingInfo
      */
-    public function hasDefaultAccount($userId){
+    public function getDefaultAccount($userId)
+    {
         $defaultAccount = BillingInfo::where('member_id', '=',$userId)
-                    ->where('is_default', '=', DB::raw('1'))
-                    ->get();
-        return !$defaultAccount->isEmpty();
+                                    ->where('is_default', '=', DB::raw('1'))
+                                    ->get();
+        return $defaultAccount;
         
     }
 
