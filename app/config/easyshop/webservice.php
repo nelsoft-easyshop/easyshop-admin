59e3ff5ea746cc11363c19efca960bd39a2188c8
unknown <inon.baguio@yahoo.com>
2014-08-19 13:24:14 +0800

    #24 Added the config file function declaration for the webservice

---
+++ app/config/easyshop/webservice.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Webservice Link
+    |--------------------------------------------------------------------------
+    |
+    | Returns the link of the homewebservice controller found in https://easyshop.ph.feature/webservice/homewebservice
+    | 
+    | 
+    |
+    */
+    'homeCmsLink' => 'https://easyshop.ph.feature/webservice/homewebservice',
+);
+
+
