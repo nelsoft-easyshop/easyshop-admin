f1ec60fcb606319f6e9c2cc4f6abc17b57d73497
unknown <inon.baguio@yahoo.com>
2014-08-27 16:52:58 +0800

    #53 Added new configs, contentWebServiceCmsLink and getContentsXML

--- app/config/easyshop/webservice.php
+++ app/config/easyshop/webservice.php
@@ -14,7 +14,9 @@ return array(
     |
     */
     'homeCmsLink' => 'https://easyshop.ph.local/webservice/homewebservice',
+    'contentWebServiceCmsLink' => 'https://easyshop.ph.local/webservice/contentwebservice',
     'getXML'=> 'https://easyshop.ph.local/webservice/homewebservice/getContents/',
+    'getContentsXML'=> 'https://easyshop.ph.local/webservice/contentwebservice/getcontents/',
     'easyShopLink'=> 'https://easyshop.ph.local',
 );
 
