172c32fa3bd2ffe61fc39c70d7db7a948fed4131
unknown <inon.baguio@yahoo.com>
2014-08-29 16:20:48 +0800

    #63 Model for the new table "es_admin_member_role"

---
+++ app/models/AdminRoles.php
@@ -0,0 +1,21 @@
+<?php
+
+class AdminRoles extends Eloquent 
+{
+
+   /**
+    * The database table used by the model.
+    *
+    * @var string
+    */
+    protected $table = 'es_admin_member_role';
+
+   /**
+    * The primary key of the table
+    *
+    */
+    protected $primaryKey = 'id_role';
+
+  
+
+}
