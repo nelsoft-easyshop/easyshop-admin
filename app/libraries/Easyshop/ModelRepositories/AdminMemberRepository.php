42640fd6442daac72f225f82834a7c792863d989
unknown <inon.baguio@yahoo.com>
2014-08-29 16:21:18 +0800

    #63 Added updateAdminRole method

--- app/libraries/Easyshop/ModelRepositories/AdminMemberRepository.php
+++ app/libraries/Easyshop/ModelRepositories/AdminMemberRepository.php
@@ -1,6 +1,7 @@
 <?php namespace Easyshop\ModelRepositories;
 
-use AdminMember;
+use Illuminate\Support\Facades\DB;
+use AdminMember, AdminRoles;
 
 class AdminMemberRepository
 {
@@ -13,6 +14,56 @@ class AdminMemberRepository
     public function getAdminMemberById($id)
     {
         return AdminMember::find($id)->pluck('password');
+    }
+
+    /**
+     * Get order by id
+     *
+     * @return Entity
+     */
+    public function getAllAdminUsers()
+    {
+        return AdminMember::all();
+    } 
+
+    /**
+     * Get all of admin roles
+     *
+     * @return Entity
+     */
+    public function getAllAdminRoles()
+    {
+        return AdminRoles::all();
+    }    
+
+    /**
+     *  Get role of a particular administrator
+     *
+     *  @param int $roleId
+     *  @return array
+     */
+    public function getAdminRoleById($roleId)
+    {
+         $query = DB::table('es_admin_member')
+         ->leftJoin('es_admin_member_role', 'es_admin_member.role_id', '=', 'es_admin_member_role.id_role')
+         ->where('es_admin_member.role_id','=', $roleId)
+         ->get();
+         return $query;
+    }  
+
+    /**
+     *  Update the role of a particular administrator
+     *
+     *  @param int $roleId
+     *  @param int $adminId     
+     *  @return array
+     */
+    public function updateAdminRole($adminId, $roleId)
+    {            
+        $adminEntity = AdminMember::find($adminId);
+        $adminEntity->role_id = $roleId;
+        $isSuccessful = $adminEntity->save();        
+        return $isSuccessful;        
     }
     
 }
