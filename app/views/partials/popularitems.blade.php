4609f6804710faf04f294885b2703cf763c7590c
unknown <inon.baguio@yahoo.com>
2014-09-08 10:55:33 +0800

    #95 removed js

--- app/views/partials/popularitems.blade.php
+++ app/views/partials/popularitems.blade.php
@@ -1,11 +1,4 @@
-    <script type="text/javascript">
-        $(document).ready(function(){
 
-            $("#loading").modal('hide');
-            $("#success").modal('show');
-        });
-
-    </script>    
         <div class="tab-pane fade active in" id="addPopularItemsDiv">
             <div class="panel-group" id="accordion">
                 <div class="panel panel-default">
