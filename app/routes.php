20e1f327c5cc1759d0c8f84f570270fecebea858
unknown <inon.baguio@yahoo.com>
2014-09-10 13:15:34 +0800

    #112 aded routes for the raffle feature

--- app/routes.php
+++ app/routes.php
@@ -39,7 +39,12 @@ Route::group(array('before' => 'auth'), function(){
     Route::put('register', array('uses' => 'AccountController@doRegister'));    
     Route::get('managerole', array('uses' => 'AccountController@showAdminLists'));    
     Route::put('adminroles', array('uses' => 'AccountController@updateAdministratorRole'));    
-    Route::put('adminactivation', array('uses' => 'AccountController@updateAdministratorActivation'));    
+    Route::put('adminactivation', array('uses' => 'AccountController@updateAdministratorActivation'));   
+
+    Route::get('raffle', array('uses' => 'RaffleManagerController@showRaffle'));    
+    Route::post('doRaffle', array('uses' => 'RaffleManagerController@doRaffle'));    
+    Route::get('showRaffleList', array('uses' => 'RaffleManagerController@showRaffleList'));    
+    Route::post('deleteRaffle', array('uses' => 'RaffleManagerController@deleteRaffle'));    
 
     Route::get('items',array('uses'=>'ProductController@showAllItems'));
     Route::get('items', array('uses' => 'ProductController@doSearchItem'));
