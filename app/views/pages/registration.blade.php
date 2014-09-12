9cab97b56126560ddefec7677481f87696d991d9
unknown <inon.baguio@yahoo.com>
2014-09-12 15:43:47 +0800

    #137 reorganized divs and removed unecessary modal

--- app/views/pages/registration.blade.php
+++ app/views/pages/registration.blade.php
@@ -52,16 +52,6 @@
 
                     @endif
             {{ Form::close() }}
-            <div class="modal fade" id="loading" >
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-body" style='text-align:center;`'>
-                            <img src="{{{ asset('images/orange_loader.gif') }}}">
-                            <h3>Please Wait..</h3>        
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="modal fade" id="success" >
                 <div class="modal-dialog">
                     <div class="modal-content">
@@ -83,57 +73,57 @@
                 </div>
             </div>      
         <div id="rolesDiv">
-        <legend>Manage Roles</legend>                  
-            <div class="table-responsive table-payment"> 
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Username</th>
-                            <th>Fullname</th>
-                            <th>Role</th>
-                            <th>Activation</th>
-                        </tr>
-                    </thead>
-                    <span style="display:none">{{$accessor=0}}</span>   
-                    @foreach($users as $adminUsers)
+            <legend>Manage Roles</legend>                  
+                <div class="table-responsive table-payment"> 
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Fullname</th>
+                                <th>Role</th>
+                                <th>Activation</th>
+                            </tr>
+                        </thead>
+                        <span style="display:none">{{$accessor=0}}</span>   
+                        @foreach($users as $adminUsers)
 
-                        <tr class="seller_detail">
-                            <td class="td_id">{{$adminUsers->id_admin_member}}</td>
-                            <td class="td_username">{{$adminUsers->username}}</td>
-                            <td class="td_fullname">{{$adminUsers->fullname}}</td>
-                            <td class="td_role" style="width:20%;">
+                            <tr class="seller_detail">
+                                <td class="td_id">{{$adminUsers->id_admin_member}}</td>
+                                <td class="td_username">{{$adminUsers->username}}</td>
+                                <td class="td_fullname">{{$adminUsers->fullname}}</td>
+                                <td class="td_role" style="width:20%;">
 
-                                <div class="btn-group">
-                                  <button class="btn btn-default dropdown-toggle" id="action{{$accessor}}">{{$specificRoles[$accessor][0]->role_name}}</button>
-                                  <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                        &nbsp;<span class="caret"></span>
-                                  </button>
+                                    <div class="btn-group">
+                                      <button class="btn btn-default dropdown-toggle" id="action{{$accessor}}">{{$specificRoles[$accessor][0]->role_name}}</button>
+                                      <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                            &nbsp;<span class="caret"></span>
+                                      </button>
 
-                                  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                                      @foreach($roles as $adminRoles)
-                                        <li><a tabindex="-1" data-role="{{ $adminRoles->role_name }}" data-id = "{{$adminUsers->id_admin_member}}" data-index="{{$index}}" data-action="{{$accessor}}" data-roleid="{{$adminRoles->id_role}}"  id="rolesLink"><span id="myspan{{$index}}">{{ $adminRoles->role_name }}</span></a></li>
-                                        <span style="display:none">{{$index++}}</span>
-                                      @endforeach
-                                  </ul>
-                                </div>
-                            </td>
-                            <td class="td_activation" style="width:20%;">
-                                <div class="btn-group btn-toggle" data-admin="{{$adminUsers->id_admin_member}}"> 
-                                    @if($adminUsers->is_active == 0)
-                                        <button class="btn btn-sm btn-default">Enabled</button>
-                                        <button class="btn btn-sm btn-primary active" >Disabled</button>
-                                    @else
-                                        <button class="btn btn-sm btn-primary active" >Enabled</button>
-                                        <button class="btn btn-sm btn-default">Disabled</button>                                    
-                                    @endif
-                                </div>
-                            </td>                           
-                        </tr>
-                    <span style="display:none">{{$accessor++}}</span>                    
-                    @endforeach 
-                </table>
-            </div>
+                                      <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+                                          @foreach($roles as $adminRoles)
+                                            <li><a tabindex="-1" data-role="{{ $adminRoles->role_name }}" data-id = "{{$adminUsers->id_admin_member}}" data-index="{{$index}}" data-action="{{$accessor}}" data-roleid="{{$adminRoles->id_role}}"  id="rolesLink"><span id="myspan{{$index}}">{{ $adminRoles->role_name }}</span></a></li>
+                                            <span style="display:none">{{$index++}}</span>
+                                          @endforeach
+                                      </ul>
+                                    </div>
+                                </td>
+                                <td class="td_activation" style="width:20%;">
+                                    <div class="btn-group btn-toggle" > 
+                                        @if($adminUsers->is_active == 0)
+                                            <button class="btn btn-sm btn-default" id="toggleMe" data-admin="{{$adminUsers->id_admin_member}}">Enabled</button>
+                                            <button class="btn btn-sm btn-primary active" id="toggleMe" data-admin="{{$adminUsers->id_admin_member}}">Disabled</button>
+                                        @else
+                                            <button class="btn btn-sm btn-primary active" id="toggleMe" data-admin="{{$adminUsers->id_admin_member}}">Enabled</button>
+                                            <button class="btn btn-sm btn-default" id="toggleMe" data-admin="{{$adminUsers->id_admin_member}}">Disabled</button>                                    
+                                        @endif
+                                    </div>
+                                </td>                         
+                            </tr>
+                        <span style="display:none">{{$accessor++}}</span>                    
+                        @endforeach 
+                    </table>
+                </div>
         </div>
 
         </div>
