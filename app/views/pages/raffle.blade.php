6a2034e01bd48b31638d2994c83fed35b35b4c2e
unknown <inon.baguio@yahoo.com>
2014-09-15 17:04:53 +0800

    #140 applied table width

--- app/views/pages/raffle.blade.php
+++ app/views/pages/raffle.blade.php
@@ -86,7 +86,7 @@
             <div id="rolesDiv">
                 <legend>Raffle Lists</legend>                  
                 <div class="table-responsive table-payment"> 
-                    <table class="table table-striped table-hover">
+                    <table class="table table-striped table-hover" width="100%">
                         <thead>
                             <tr>
                                 <th>/</th>
@@ -99,18 +99,21 @@
 
                         @foreach($raffles as $raffle)
                             <tr class="seller_detail">
-                                <td class="td_id">
+                                <td class="td_id" width="5%">
                                     <a href="javascript:void(0)" id="delete" data-id = "{{$raffle->raffle_id}}"><span class="glyphicon glyphicon-remove"></span></a>
                                 </td>
-                                <td class="td_username">{{ $raffle->raffle_name }}</td>
-                                <td class="td_username">
+                                <td class="td_username" width="20%">{{ $raffle->raffle_name }}</td>
+                                <td class="td_username" width="25%">
 
                                     <div id="winnersDisplay">{{ str_ireplace(",","<br/>",$raffle->winners) }}</div>
 
 
                                 </td>
-                                <td class="td_username">{{ $raffle->created_at }}</td>
-                                <td class="td_fullname">{{ $raffle->prices }}</td>                                              
+                                <td class="td_username" width="25%">{{ $raffle->created_at }}</td>
+                                <td class="td_fullname"width="25%">
+
+                                    <div id="pricesDisplay">{{ str_ireplace(",","<br/>",$raffle->prices) }}
+                                </td>                                              
                             </tr>
                         @endforeach                         
 
