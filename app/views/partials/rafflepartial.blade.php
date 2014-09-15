f940787a6e937530dd772022e4cf41820f9449b8
unknown <inon.baguio@yahoo.com>
2014-09-15 15:21:02 +0800

    #140 added time created column

--- app/views/partials/rafflepartial.blade.php
+++ app/views/partials/rafflepartial.blade.php
@@ -7,6 +7,7 @@
                                 <th>/</th>
                                 <th>Raffle_Name</th>
                                 <th>Winners</th>
+                                <th>Time Created</th>
                                 <th>Prices</th>
                             </tr>
                         </thead>
@@ -14,16 +15,22 @@
                         @foreach($raffles as $raffle)
                             <tr class="seller_detail">
                                 <td class="td_id">
-
-
-                                <a id="delete" data-id = "{{$raffle->raffle_id}}"><span class="glyphicon glyphicon-remove"></span></a>
+                                    <a href="javascript:void(0)" id="delete" data-id = "{{$raffle->raffle_id}}"><span class="glyphicon glyphicon-remove"></span></a>
                                 </td>
                                 <td class="td_username">{{ $raffle->raffle_name }}</td>
-                                <td class="td_username">{{ $raffle->winners }}</td>
-                                <td class="td_fullname">{{ $raffle->prices }}</td>                          
+                                <td class="td_username">
+
+                                    <div id="winnersDisplay">{{ str_ireplace(",","<br/>",$raffle->winners) }}</div>
+
+
+                                </td>
+                                <td class="td_username">{{ $raffle->created_at }}</td>
+                                <td class="td_fullname">{{ $raffle->prices }}</td>                                              
                             </tr>
                         @endforeach                         
 
                     </table>
                 </div>
             </div>
+  {{ HTML::script('js/raffle.js') }}      
+        
