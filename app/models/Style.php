93db9fb14bc7266fabc96bfa6db63534515f1dcc
unknown <inon.baguio@yahoo.com>
2014-09-23 16:49:31 +0800

    #159 added model declaration for Style

---
+++ app/models/Style.php
@@ -0,0 +1,15 @@
+<?php
+
+class Style extends Eloquent 
+{
+
+   /**
+    * The database table used by the model.
+    *
+    * @var string
+    */
+    protected $table = 'es_style';
+    protected $primaryKey = 'id_style';
+    public $timestamps = false;
+}
+
