3f23c1f0d7c108d0037f56b835f69cfbff3c4a57
unknown <inon.baguio@yahoo.com>
2014-09-23 16:49:19 +0800

    #159 view for product csv upload

---
+++ app/views/pages/productcsv.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.default')
+
+@section('description', 'Upload Product CSV')
+@section('keywords', '')
+@section('title', 'Product CSV Upoad | Easyshop Admin')
+@section('header_tagline', 'Product CSV Upload Manager.')
+
+@section('page_header')
+    @include('includes.header')
+    <link type="text/css" href="{{{ asset('css/style.css')  }}}" rel="stylesheet"  media="screen"/>
+    <link type="text/css" href="{{{ asset('css/src/fileinput.css') }}}" rel="stylesheet"  media="screen"/>
+
+@stop
+
+@section('content')
+<div id="mainsection">
+
+        <div class='registration_form' id="registration_form">
+            {{ Form::open(array('url' => 'productcsv','enctype'=>'multipart/form-data', "id" => "uploadData")) }}
+                <legend>Upload Product Excel/CSV Data </legend>
+                <div class="form-group">
+                    {{ Form::file('image[]', ['multiple' => true, 'class' => 'file ', 'id' => 'uploadCSV','data-preview-file-type' => 'any']) }}
+                </div>
+            {{ Form::close() }}
+            <a href="{{{ asset('misc/CSV_Upload.xlsx') }}}" class="btn btn-default btn-sm" id="download-btn"><span class="glyphicon glyphicon-floppy-disk"></span>&nbsp;Download Template</a>
+        </div>
+        <div class='help-block text-center disp_error'><h5>{{ $errors->first('noinput') }}</h5></div>
+        @if(isset($queryError))
+            <div style="color:red;text-align:center;"><h5>Kindly check and fill up all fields with approriate values in the file</h5></div>
+        @endif
+
+        @if(isset($result))
+            <div class='registration_form' id="imageforms">
+                {{ Form::open(array('url' => "#",'enctype'=>'multipart/form-data','id'=>'uploadphoto','method'=>'post')) }}
+                    <legend>Upload Product Images</legend>
+
+                        @foreach($result as $data => $key)
+                           <input name="filenames[]" type="hidden" value="{{$key}}"/>
+                           <input name="time[]" type="hidden" value="{{round(microtime(true) * 1000) + $data}}"/>
+                        @endforeach
+                    <div class="form-group">
+                        {{ Form::file('image[]', ['multiple' => true, 'class' => 'file file-loading', 'id' => 'uploadImageOnly','data-preview-file-type' => 'any','accept' => "image/*"]) }}
+                    </div>
+                {{ Form::close() }}
+            </div>        
+        @endif
+
+        <div class="modal fade" id="success" >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body" style='text-align:center;'>
+                        <img src="{{{ asset('images/es_32x32.png') }}}">
+                        <h3>Success</h3>        
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+@stop
+
+@section('page_js')
+ 
+{{ HTML::script('js/productcsv.js') }}
+{{ HTML::script('js/src/jquery.dataTables.js') }}
+{{ HTML::script('js/src/fileinput.js') }}
+{{ HTML::script('js/src/jquery.form.js') }}
+
+
+@stop
