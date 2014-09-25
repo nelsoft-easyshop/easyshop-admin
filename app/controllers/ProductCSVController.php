b3d75df1aed21dfbbebcba53a9ee37a4dcbfb82a
unknown <inon.baguio@yahoo.com>
2014-09-25 18:56:38 +0800

    #159 changed checktData method call to insertData method call

--- app/controllers/ProductCSVController.php
+++ app/controllers/ProductCSVController.php
@@ -58,7 +58,7 @@ class ProductCSVController extends BaseController
         foreach($files as $file) {
 
                     $reader = $excel->load("./public/misc/$file"); 
-                    $result = $productCSVRepo->checkData($reader->get());
+                    $result = $productCSVRepo->insertData($reader->get());
                     $data[]  = $result;
                     if (File::exists($destinationPath.$file)) {
                         File::delete($destinationPath.$file);
