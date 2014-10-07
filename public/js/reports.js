47eed1e4b39b0672282b2f1af090d47638f1aadb
inon <inon.baguio@yahoo.com>
2014-10-07 23:57:15 +0800

    #169 added initialization of csv exports

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,25 +1,47 @@
 (function ($) {    
     $("#table1").DataTable({
-            "order": [0,'desc'],
-            "bPaginate": false,
-
+            "order": [0,'asc'],
+            "dom": 'T<"clear">lfrtip',
+            "tableTools": {
+                "sSwfPath": "misc/TableTools/copy_csv_xls_pdf.swf",
+                "aButtons": [ "copy", "csv" ]                
+            },
         });      
     $("#table2").DataTable({
             "bSort" : false,
-            paging: false,
-            "bFilter": false
+            "bPaginate": false,
+            "dom": 'T<"clear">lfrtip',
+            "tableTools": {
+                "sSwfPath": "misc/TableTools/copy_csv_xls_pdf.swf",
+                "aButtons": [ "copy", "csv" ]                
+            },
 
         }); 
     $("#table3").DataTable({
             "order": [0,'desc'],
-            paging: false
-
+            "bPaginate": false,
+            "dom": 'T<"clear">lfrtip',
+            "tableTools": {
+                "sSwfPath": "misc/TableTools/copy_csv_xls_pdf.swf",
+                "aButtons": [ "copy", "csv" ]                
+            },
         });    
     $("#table5").DataTable({
-            paging: false
+            "bPaginate": false,
+            "dom": 'T<"clear">lfrtip',
+            "tableTools": {
+                "sSwfPath": "misc/TableTools/copy_csv_xls_pdf.swf",
+                "aButtons": [ "copy", "csv" ]                
+            },
+
         });
     $("#table4").DataTable({
-            paging: false
+            "bPaginate": false,
+            "dom": 'T<"clear">lfrtip',
+            "tableTools": {
+                "sSwfPath": "misc/TableTools/copy_csv_xls_pdf.swf",
+                "aButtons": [ "copy", "csv" ]                
+            },             
 
         });                                
 })(jQuery);
