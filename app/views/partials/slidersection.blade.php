14ccce775dd89aa200b24c33cd200d97f71c9c45
unknown <inon.baguio@yahoo.com>
2014-10-17 14:18:23 +0800

    #177 changed styles for images block

--- app/views/partials/slidersection.blade.php
+++ app/views/partials/slidersection.blade.php
@@ -20,7 +20,7 @@
                     </div>                                                           
                     <div class="form-group">
                         <div class="col-xs-offset-2 col-xs-10">
-                            <a1 href="#"  class="btn btn-primary text-center" data-url = "{{{$newHomeCmsLink}}}/addSliderSection" data-subcategories = "#subcategories_test" id="addMainSlider">Add Sub Category Navigation</a>
+                            <a1 href="#"  class="btn btn-primary text-center" data-url = "{{{$newHomeCmsLink}}}/addSliderSection" data-subcategories = "#subcategories_test" id="addMainSlider">Add Main Slider</a>
                         </div>
                     </div>                                      
                 </form>
@@ -129,12 +129,12 @@
                                             <span style="display:none;">{{$slideCount=1}}</span>  
                                             @foreach($slides->image as $subSlides)
                                                 <div style="position:relative;display:inline-block;">
-                                                    <div class='well' style="height:210px;">
-                                                        <p>
-                                                            <img src="{{$easyShopLink}}/assets//cms/home/{{$subSlides->path}}" data-div="" style="width:250px !important;height:150px !important; border: black 1px solid;" class='img-responsive'/>
-                                                        </p>
+                                                    <div class='well' style="height:auto;">
+                                                        <div style="width: 200px; height: 220px;max-width: 200px; max-height: 250px; display: table-cell; vertical-align: middle;">
+                                                            <img src="{{$easyShopLink}}{{$subSlides->path}}" class="img-responsive" data-div="" style="border: black 1px solid; width: 100%; height: auto; max-height: 200px;"/>
+                                                        </div>
 
-                                                        <a href="#myMain_{{$sliderIndex}}_{{$subSlideIndex}}" data-toggle="modal" style="position:absolute;top:180px;left:135px;"><span class="glyphicon glyphicon-edit" style="font-size:16px;"></span></a>
+                                                        <a href="#myMain_{{$sliderIndex}}_{{$subSlideIndex}}" data-toggle="modal" style="position:absolute;top:235px;left:112px;"><span class="glyphicon glyphicon-edit" style="font-size:16px;"></span></a>
                                                         <a class="btn btn-default" 
                                                             id="removeSubSlide" 
                                                             data-index="{{$sliderIndex}}" 
@@ -150,7 +150,7 @@
                                                             data-index="{{$sliderIndex}}" 
                                                             data-order="{{$subSlideIndex}}" 
                                                             data-subindex="{{$subSlideIndex}}" 
-                                                            style="position:absolute;top:180px;left:5px;"
+                                                            style="position:absolute;top:235px;left:5px;"
                                                             data-url = "{{{$newHomeCmsLink}}}/setSliderPosition"
                                                          ><span class="glyphicon glyphicon-chevron-left pull-left" style="font-size:16px;"></span></a>
 
@@ -160,7 +160,7 @@
                                                             data-index="{{$sliderIndex}}" 
                                                             data-order="{{$subSlideIndex}}" 
                                                             data-subindex="{{$subSlideIndex}}" 
-                                                            style="position:absolute;top:180px;right:5px;"
+                                                            style="position:absolute;top:235px;right:5px;"
                                                             data-url = "{{{$newHomeCmsLink}}}/setSliderPosition"
                                                          ><span class="glyphicon glyphicon-chevron-right pull-right" style="font-size:16px;"></span></a>
                                                      </div>
