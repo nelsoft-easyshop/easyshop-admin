5f0d41e12739537a5cdd215e11bcaad0c97c7dfc
unknown <inon.baguio@yahoo.com>
2014-10-28 16:33:01 +0800

    #207 partial js logic

--- public/js/payoutsbuyer.js
+++ public/js/payoutsbuyer.js
@@ -1,21 +1,63 @@
 (function ($) {
 
 
-    $('.transaction-list td').click(function(){
-        BootstrapDialog.show({
-            title: 'Order Details',
-            cssClass: 'order-detail-modal',
-            message: "test",
-            buttons: [{
-                label: 'Close',
-                action: function(dialogRef) {
-                    dialogRef.close();
+    $('.buyer-list').click(function(){
+        loader.showPleaseWait(); 
+        var url = "/payout-buyer/view-transaction-details";
+        var orderId = $(this).find('.td_order_id').html(); 
+        var $request = $.ajax({
+                url: url,
+                data:{order_id:orderId},
+                type: 'get',
+                dataType: 'JSON',
+                success: function(result){
+                    var modal_container = $('<div></div>');
+                    modal_container.append(result.html); 
+                    loader.hidePleaseWait();
+                    BootstrapDialog.show({
+                        title: 'History Information',
+                        message: modal_container,
+                        cssClass: 'payment-dialog',
+                        buttons: [
+                            {
+                                id: "tagOrder",
+                                label: 'Save',
+                                action: function(dialogRef) {
+                                    dialogRef.close();
+                                }
+                            },
+                            {
+                                label: 'Back',
+                                action: function(dialogRef) {
+                                    dialogRef.close();
+                                }
+                            }
+                        ]
+                    });
                 }
-            }]
+            });
+    });
+
+    $(document.body).on('click','#tagOrder',function(){
+        var orderProductIdCollection =  [];
+
+        var tag =  $('#tagOption option:selected').val();
+        $('.orderProductId').each(function(){
+            orderProductIdCollection.push( parseInt($(this).html().trim(), 10));
         });
         
+        $.ajax({
+            url: url,
+            data: {order_product_ids: orderProductIdCollection},
+            type: 'post',
+            dataType: 'JSON',                      
+            success: function(result){
+                spinner.stop();
+                    
+            }
+        });
+       
     });
-    
     
 })(jQuery);
 
