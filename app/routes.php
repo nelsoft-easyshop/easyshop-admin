4d4136153a1a03ff8dfb2246ea1e688b4fc25ca3
unknown <inon.baguio@yahoo.com>
2014-10-29 15:05:32 +0800

    #207 changed contact-buyer to update-buyer-transaction

--- app/routes.php
+++ app/routes.php
@@ -111,7 +111,7 @@ Route::group(array('before' => 'auth'), function(){
     Route::get('payout/seller/view-transactions-details', array('uses' => 'OrderProductController@getSellerTransactionDetailsByOrderId'));
     Route::get('payout-buyer', array('uses' => 'OrderProductController@getOrderProductsContactBuyer'));   
     Route::get('/payout-buyer/view-transaction-details', array('uses' => 'OrderProductController@getBuyerTransactionDetailsByOrderId'));   
-    Route::get('payout-buyer/contact-buyer', array('uses' => 'OrderProductController@insertContactedBuyer'));   
+    Route::get('payout-buyer/update-buyer-transaction', array('uses' => 'OrderProductController@updateBuyerTagTransaction'));   
     Route::get('payout/seller/update-transaction', array('uses' => 'OrderProductController@updateOrderProductStatus'));
 
 
