8d172d48265ee08ec1ce540226b2639a86c8cb0e
unknown <inon.baguio@yahoo.com>
2014-10-29 20:59:46 +0800

    #207 added payout-buyers-sort route

--- app/routes.php
+++ app/routes.php
@@ -110,6 +110,7 @@ Route::group(array('before' => 'auth'), function(){
     Route::get('payout/seller/view-transactions', array('uses' => 'OrderProductController@getSellerExistingTransaction'));
     Route::get('payout/seller/view-transactions-details', array('uses' => 'OrderProductController@getSellerTransactionDetailsByOrderId'));
     Route::get('payout-buyer', array('uses' => 'OrderProductController@getOrderProductsContactBuyer'));   
+    Route::get('payout-buyer-sort', array('uses' => 'OrderProductController@searchContactBuyer'));   
     Route::get('/payout-buyer/view-transaction-details', array('uses' => 'OrderProductController@getBuyerTransactionDetailsByOrderId'));   
     Route::get('payout-buyer/update-buyer-transaction', array('uses' => 'OrderProductController@updateBuyerTagTransaction'));   
     Route::get('payout/seller/update-transaction', array('uses' => 'OrderProductController@updateOrderProductStatus'));
