21aa8dc9609f6d11454fa4253f14715c97536f1f
unknown <inon.baguio@yahoo.com>
2014-10-29 20:59:27 +0800

    #207 added filtering interface enhancements

--- app/views/pages/payoutsbuyers.blade.php
+++ app/views/pages/payoutsbuyers.blade.php
@@ -12,32 +12,47 @@
 
 
 @section('content')
+
   <div id="mainsection">
         <br/>   
 
         <div class="table-responsive table-payment transaction-list"> 
-        
-            <table class="table table-striped table-hover">
+            <br/>
+            <table id="table1" class="table table-striped table-hover">
+
                 <thead>
                      <tr>
-                        <th>Order ID</th>
-                        <th>Buyer Name</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Contact Number</th>
+                        <th  style="cursor:pointer;">Order ID <span class="glyphicon glyphicon-chevron-up" data-sortby='es_order.id_order' data-sortorder='asc'></span>
+                          {{ Form::open(array('url' => 'payout-buyer-sort', 'id' => 'orderForm', 'method' => 'get')) }}
+                            <input type='hidden' name='page' id="pageNumber" value="1">
+                            <input type='hidden' name="sortBy" id="sortBy" value=""/>
+                            <input type='hidden' name="sortOrder" id="sortOrder" value=""/>
+                          {{ Form::close() }}
+                        </th>
+                        <th>Buyer Name <span class="glyphicon glyphicon-chevron-up"  style="cursor:pointer;" data-sortby='es_member.username' data-sortorder='asc'></span></th>
+                        <th>Email <span class="glyphicon glyphicon-chevron-up"  style="cursor:pointer;" data-sortby='es_member.email' data-sortorder='asc'></span></th>
+                        <th>Status <span class="glyphicon glyphicon-chevron-up"  style="cursor:pointer;" data-sortby='es_tag_type.tag_description' data-sortorder='asc'></span></th>
+                        <th>Contact Number<span class="glyphicon glyphicon-chevron-up"  style="cursor:pointer;" data-sortby='es_member.contactno' data-sortorder='asc'></span></th>
                      </tr>
                 </thead>
-                @foreach($orders as $key => $details)
-                    <tr class='buyer-list'>
-                        <td class='td_order_id'>{{{$details->id_order}}}</td>
-                        <td class='username' data-memberid="{{{ $details->seller_id }}}">{{{$details->username}}}</td>
-                        <td class='email'>{{{$details->email}}}</td>
-                        <td class='email'><span class="org_btn view"> {{{ ($details->tag_description) ? $details->tag_description : 'PENDING'  }}} </span></td>
-                        <td class="contactno">{{{ ($details->contactno) ? $details->contactno : 'N/A' }}}</td>                        
-                    </tr>
-                @endforeach
+                <tbody id="tbody">
+                  @foreach($orders as $details)
+                      <tr class='buyer-list'>
+                          <td class='td_order_id'>{{{$details->id_order}}}</td>
+                          <td class='username' data-memberid="{{{ $details->seller_id }}}">{{{$details->username}}}</td>
+                          <td class='email'>{{{$details->email}}}</td>
+                          <td class='email'><span class="org_btn view" style="background-color:{{  $details->tag_color  }};">{{{ ($details->tag_description) ? $details->tag_description : 'PENDING'  }}} </span></td>
+                          <td class="contactno">{{{ ($details->contactno) ? $details->contactno : 'N/A' }}}</td>                        
+                      </tr>
+                  @endforeach
+                </tbody>
             </table>
+            {{ $orders->links() }}
+
         </div>
+
+
+
       </div>
     
     
