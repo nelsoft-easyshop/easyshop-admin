e7607046d7c4a968194fa8238cd253a9dba292e4
unknown <inon.baguio@yahoo.com>
2014-11-03 15:00:36 +0800

    #207 moved getting the untagged transactions to repositories

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,29 +10,14 @@ class HomeController extends BaseController
     public function index()
     {
 
-        $untaggedBuyerTransactions = 0;        
-        $orderProductRepository = App::make('OrderProductRepository'); 
-        $orderProductTagRepositoryRepository = App::make('OrderProductTagRepository'); 
-
-        foreach (array_flatten($orderProductRepository->getBuyersTransactionWithShippingComment(null, null, null, null)) as $value) {
-            $dt = Carbon::create(Carbon::parse($value->expected_date)->year
-                                , Carbon::parse($value->expected_date)->month
-                                , Carbon::parse($value->expected_date)->day);
-
-            $exists = $orderProductTagRepositoryRepository->checkTaggerOrderProduct($value->id_order_product);
-            
-            if(Carbon::now() > $dt->addDays(2) && $exists < 1){
-                $untaggedBuyerTransactions++;
-            }
-        }
-
         $orderRepository = App::make('OrderProductRepository'); 
-        $ungTagCount = $orderRepository->countUntagTransaction();
+        $unTaggedSellersTransaction = $orderRepository->countUntagTransaction();
+        $untaggedBuyerTransactionsCount  = $orderRepository->countUntagTransaction(FALSE);
 
         return View::make('pages.dashboard')
             ->with('username', Auth::user()->username)
-            ->with("untaggedBuyerTransactions",$untaggedBuyerTransactions)
-            ->with('unTagCount',$ungTagCount);
+            ->with("untaggedBuyerTransactionsCount",$untaggedBuyerTransactionsCount )
+            ->with('unTaggedSellersTransaction',$unTaggedSellersTransaction);
 
     }
 
