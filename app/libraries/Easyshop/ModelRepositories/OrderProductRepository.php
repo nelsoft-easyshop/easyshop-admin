6da88ee9362698c965cbc4e3f7eb17bc80825d60
unknown <inon.baguio@yahoo.com>
2014-11-06 12:40:40 +0800

    #207 refined query for getBuyersTransactionWithShippingComment()

--- app/libraries/Easyshop/ModelRepositories/OrderProductRepository.php
+++ app/libraries/Easyshop/ModelRepositories/OrderProductRepository.php
@@ -2,7 +2,7 @@
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use OrderProduct, OrderProductStatus, OrderStatus, OrderProductHistory, PaymentMethod, OrderProductTag, TagType;
+use OrderProduct, OrderProductStatus, OrderStatus, OrderProductHistory, PaymentMethod, OrderProductTag, TagType, ProductShippingComment;;
 
 
 class OrderProductRepository extends AbstractRepository
@@ -295,15 +295,15 @@ class OrderProductRepository extends AbstractRepository
     {
         $sortBy = $sortBy === NULL ? "es_order.dateadded" : $sortBy;
         $sortOrder = $sortOrder === NULL ? "DESC" : $sortOrder;
-        $query = OrderProduct::leftJoin('es_order','es_order_product.order_id', '=', 'es_order.id_order'); 
+        $query = ProductShippingComment::leftJoin("es_order_product", "es_order_product.id_order_product", "=", "es_product_shipping_comment.order_product_id");
+        $query->leftJoin('es_order','es_order_product.order_id', '=', 'es_order.id_order'); 
+        $query->leftJoin("es_member","es_order.buyer_id","=","es_member.id_member");
         $query->leftJoin("es_order_product_tag","es_order_product_tag.order_product_id","=","es_order_product.id_order_product");
         $query->leftJoin("es_tag_type","es_tag_type.id_tag_type","=","es_order_product_tag.tag_type_id");
-        $query->leftJoin("es_member","es_order.buyer_id","=","es_member.id_member");
-        $query->rightJoin("es_product_shipping_comment","es_product_shipping_comment.order_product_id","=","es_order_product.id_order_product");
-        $query->where('es_order.order_status', '=', OrderStatus::STATUS_PAID)
-                ->where("es_order_product_tag.tag_type_id","!=", TagType::PAYOUT)
-                ->orWhere("es_order_product_tag.tag_type_id","=", NULL)
-                ->whereIn('es_order.payment_method_id',[PaymentMethod::PAYPAL,PaymentMethod::DRAGONPAY]);
+        $query->where('es_order.order_status', '=', OrderStatus::STATUS_PAID);
+        $query->whereIn('es_order.payment_method_id',[PaymentMethod::PAYPAL,PaymentMethod::DRAGONPAY]);
+        $query->whereNull('es_order_product_tag.tag_type_id');        
+        $query->orWhere('es_order_product_tag.tag_type_id', '!=', TagType::PAYOUT);
         if($filter != NULL) {
             if($filter == "username") {
                 $query->where('es_member.username', 'LIKE', '%' . $filterBy . '%');
