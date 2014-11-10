ec2a9f6af3547ba941f652788ba9f142dfb09f49
Sam <samgavinio@easyshop.ph>
2014-11-10 12:07:05 +0800

    #259 Grouped together modules

--- app/views/pages/dashboard.blade.php
+++ app/views/pages/dashboard.blade.php
@@ -40,38 +40,14 @@
 
     <div id="mainsection">	
         <ul class="featurebox">
-            <li>
-                <a href='/users'>
-                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
-                    <span >Registered Users</span>
-                </a>
-                <p> View the complete list of registered users and their personal details. Manage and update various user information.</p>
-            
-            </li>
-            <li>	
+            <div class="section-title"> TRANSACTION MANAGEMENT </div>
+           
+            <li>    
                 <a href="/transaction">
                     <img src="{{{ asset('images/icon_list.png') }}}"/> 
                     <span>Transactions List</span>
                 </a>
                 <p> Monitor and manage on-going transactions on the site. Generate transactions record spreadsheets & void invalid transactions </p>
-            </li>
-            
-            <li>	
-                <a href="/items">
-                    <img src="{{{ asset('images/icon_product.png') }}}"/> 
-                    <span>Product Listings</span>
-                </a>
-                <p>View all active listings on the site for all users. Search for specific items and view the available product details.</p>
-            </li>
-            
-            <li>	
-                <a href="/category">
-                    <img src="{{{ asset('images/icon_category.png') }}}"/> 
-                    <span>Category Listing</span>
-                </a>
-                <p>
-                    Manage and update all available categories on the site. Add, edit, remove and change the sor order of different categories. 
-                </p>
             </li>
             
             <li>
@@ -91,7 +67,8 @@
                 <p> View details about refunds for buyers and update the status of the transaction</p>
             
             </li>
-
+            
+            
             <li>
                 <a href='/payout/seller'>
                     <img src="{{{ asset('images/icon_people.png') }}}"/> 
@@ -108,8 +85,17 @@
                 </a>
                 <p>View buyers that have to be contacted</p>
             
-            </li>            
-
+            </li>    
+            
+            <li>
+                <a href='/messages'>
+                    <img src="{{{ asset('images/icon_category.png') }}}"/> 
+                    <span >Partner Messaging</span>
+                </a>
+                <p>Centralize all incoming messages from partner accounts that we're handling to a central account.</p>
+            </li> 
+            
+            <div class="section-title"> CONTENT MANAGEMENT</div>
             <li>
                 <a href='/home'>
                     <img src="{{{ asset('images/icon_category.png') }}}"/> 
@@ -118,6 +104,7 @@
                 <p>Manage the home page of Easyshop.ph. View and updates its different web parts.</p>
             
             </li>   
+            
             <li>
                 <a href='/cms/feeds'>
                     <img src="{{{ asset('images/icon_category.png') }}}"/> 
@@ -134,7 +121,66 @@
                 </a>
                 <p>Content Manager for Mobile Home page. View and edit Box Contents and home page main slides</p>
 
-            </li>              
+            </li>      
+            
+            
+            
+            <div class="section-title"> EASYSHOP.PH DATA</div>
+            <li>
+                <a href='/users'>
+                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
+                    <span >Registered Users</span>
+                </a>
+                <p> View the complete list of registered users and their personal details. Manage and update various user information.</p>
+            
+            </li>
+        
+            
+            <li>	
+                <a href="/items">
+                    <img src="{{{ asset('images/icon_product.png') }}}"/> 
+                    <span>Product Listings</span>
+                </a>
+                <p>View all active listings on the site for all users. Search for specific items and view the available product details.</p>
+            </li>
+            
+            <li>	
+                <a href="/category">
+                    <img src="{{{ asset('images/icon_category.png') }}}"/> 
+                    <span>Category Listing</span>
+                </a>
+                <p>
+                    Manage and update all available categories on the site. Add, edit, remove and change the sor order of different categories. 
+                </p>
+            </li>
+            <li>    
+                <a href="/searchkeywords">
+                    <img src="{{{ asset('images/icon_list.png') }}}"/> 
+                    <span>View Search Keywords</span>
+                </a>
+                <p> View search keywords used by the users.</p>
+            </li> 
+            
+            <li>
+                <a href='/productcsv'>
+                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
+                    <span >Product CSV Upload</span>
+                </a>
+                <p> Enables the administrator to upload multiple product images and product info through csv/excel files.</p>
+            
+            </li>  
+            
+            <li>
+                <a href='/reports'>
+                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
+                    <span >Reports Manager</span>
+                </a>
+                <p> View Users and Products overall statistics</p>
+
+            </li>
+            
+            
+            <div class="section-title"> ACCOUNT MANAGEMENT</div>
             <li>
                 <a href='/register'>
                     <img src="{{{ asset('images/icon_category.png') }}}"/> 
@@ -150,37 +196,11 @@
                 </a>
                 <p> Add and view raffles. Upload raffle member CSV files or manually input pool of members for the raffle.</p>
             </li>   
-            <li>    
-                <a href="/searchkeywords">
-                    <img src="{{{ asset('images/icon_list.png') }}}"/> 
-                    <span>View Search Keywords</span>
-                </a>
-                <p> View search keywords used by the users.</p>
-            </li> 
-            <li>
-                <a href='/messages'>
-                    <img src="{{{ asset('images/icon_category.png') }}}"/> 
-                    <span >Partner Messaging</span>
-                </a>
-                <p>Centralize all incoming messages from partner accounts that we're handling to a central account.</p>
-            </li> 
-            <li>
-                <a href='/productcsv'>
-                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
-                    <span >Product CSV Upload</span>
-                </a>
-                <p> Enables the administrator to upload multiple product images and product info through csv/excel files.</p>
-            
-            </li>     
-            <li>
-                <a href='/reports'>
-                    <img src="{{{ asset('images/icon_people.png') }}}"/> 
-                    <span >Reports Manager</span>
-                </a>
-                <p> View Users and Products overall statistics</p>
-
-            </li>
+      
         </ul>
     </div>
+    
+    
+    
 
 @stop
