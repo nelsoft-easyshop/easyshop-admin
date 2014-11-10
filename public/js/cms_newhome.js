96e0d602b781b3321dacad61967f35094eca8d19
unknown <inon.baguio@yahoo.com>
2014-11-10 20:21:34 +0800

    #270 declared 2 to a javaScript variable

--- public/js/cms_newhome.js
+++ public/js/cms_newhome.js
@@ -1,7 +1,7 @@
 (function () {
     var userid = $("#userid").val();
     var password = $("#password").val();
-
+    var minimumCategoryProductPanel = 2;
     $(document.body).on('click','#addSubCategorySection',function (e) { 
         loader.showPleaseWait();          
         var index = $(this).closest("form").find("#index").val();
@@ -167,12 +167,11 @@
         var nodename = $(this).data("nodename");
         var url = $(this).data("url");
         var hash = hex_sha1(index + subindex  + nodename + userid + password);
-
         data = { index: index, subIndex:subindex,nodename:nodename, userid:userid,  password:password, hash:hash, callback:'?'};  
         var count = parseInt($(".categoryProductPanelCount_"+index).last().text());        
         var tableSelector = "#categorySectionProductPanel_" + index;
         var reloadurl = "getCategoriesProductPanel/" + index;
-        if(count > 2 ) {
+        if(count > minimumCategoryProductPanel ) {
             loader.showPleaseWait();                    
             $.ajax({
                 type: 'GET',
@@ -354,7 +353,7 @@
         var reloadurl = "getSubCategoriesSection/" + index;
         var categoryCount = ".subCategorySectionCount_"+index;
         var count = $(categoryCount).last().text();
-        if(count > 2) {
+        if(count > minimumCategoryProductPanel) {
             $.ajax({
                 type: 'GET',
                 url: url,
