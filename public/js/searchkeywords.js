caba7ee2b0d712b3decb4a71f360fa172cea33ed
unknown <inon.baguio@yahoo.com>
2015-01-13 16:10:14 +0800

    #376 removed unused js function

--- public/js/searchkeywords.js
+++ public/js/searchkeywords.js
@@ -1,20 +1,5 @@
 (function ($) {
 
-    $("form").submit(function (e){
-        e.preventDefault();
-    });
-    $( "#searchBox" ).keyup(function() {
-        var keyword = $(this).val();
-        var order = $("#order").val();
-        $.ajax({
-            type: 'post',
-            url: "/customsearchkeywords",
-            data:{keyword:keyword,order:order},
-            dataType: 'json',
-            success: function(json) {
-                $("#table_keywords").html(json.html);
-            }
-        });           
-    });
 
 })(jQuery);    
+
