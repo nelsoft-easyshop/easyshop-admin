55741428b6b56f2ef044eceaefa10b9234c0f459
unknown <inon.baguio@yahoo.com>
2015-01-21 15:50:29 +0800

    #392 fixed indention

--- public/js/cms_newhome.js
+++ public/js/cms_newhome.js
@@ -8,13 +8,12 @@
         }
     });
 
-     $(window).keydown(function(event){
+    $(window).keydown(function(event){
         if(event.keyCode == 13) {
           event.preventDefault();
           return false;
         }
     });    
-
 
     var userid = $("#userid").val();
     var password = $("#password").val();
