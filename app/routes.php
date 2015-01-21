34721ea022996b3540178302aa229630fbc59f30
unknown <inon.baguio@yahoo.com>
2015-01-21 18:15:43 +0800

    #395 added route for reset password

--- app/routes.php
+++ app/routes.php
@@ -59,6 +59,8 @@ Route::group(array('before' => 'auth'), function(){
     Route::get('managerole', array('uses' => 'AccountController@showAdminLists'));    
     Route::put('adminroles', array('uses' => 'AccountController@updateAdministratorRole'));    
     Route::put('adminactivation', array('uses' => 'AccountController@updateAdministratorActivation'));   
+    Route::put('getadminaccount', array('uses' => 'AccountController@getAdminAccount'));   
+    Route::put('resetPassword', array('uses' => 'AccountController@resetPassword'));   
 
     Route::get('raffle', array('uses' => 'RaffleManagerController@showRaffle'));    
     Route::post('doRaffle', array('uses' => 'RaffleManagerController@doRaffle'));    
