b86dc04d87576604ae4064c1e7bd682456ff3d07
Sam <samgavinio@easyshop.ph>
2015-02-10 19:03:11 +0800

    #459 added config for role route whitelist

---
+++ app/config/easyshop/role-route-whitelist.php
@@ -0,0 +1,27 @@
+<?php 
+
+    $pages['transactionManagement'] = [
+        "transaction", 
+        "contact",
+        "messages"
+    ];
+    
+    $pages['contentManagement'] = [
+        "cms",
+    ];
+    
+    $pages['dataManagement'] = [
+        "users", 
+        "items", 
+        "category", 
+        "searchkeywords", 
+        "productcsv",
+        "reports",
+        "raffle"
+    ];
+    
+    $pages['accountManagement'] = [
+        "register"
+    ];
+    
+    return $pages;
