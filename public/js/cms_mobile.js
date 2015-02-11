5f8c87f43a7c3cc9485d9d6657406c1643d211c3
unknown <inon.baguio@yahoo.com>
2015-02-11 19:59:25 +0800

    #465 refactored adding box content details

--- public/js/cms_mobile.js
+++ public/js/cms_mobile.js
@@ -277,6 +277,7 @@
     });
 
     $(document.body).on('click','#addBoxContent',function (e) { 
+        var tableIndex = $(this).closest("form").find("#index").val();
         var value = $(this).closest("form").find("#value").val();
         var type = $(this).closest("form").find("#type").val();
         var target = $(this).closest("form").find("#target").val();
@@ -293,12 +294,12 @@
         }
         else {
             loader.showPleaseWait();
-            addBoxContent(data, url, boxIndex, sectionIndex, value, type, target, actionType);
+            addBoxContent(data, url, boxIndex, sectionIndex, value, type, target, actionType, tableIndex);
         }
 
     });    
 
-    function addBoxContent(data, url, boxIndex,sectionIndex, value, type, target, actionType)
+    function addBoxContent(data, url, boxIndex,sectionIndex, value, type, target, actionType, tableIndex)
     {
   
         $.ajax({
@@ -316,7 +317,9 @@
                 }
                 else {
                     loader.hidePleaseWait();   
-                    appendDataContainer(url, boxIndex, sectionIndex, value, type, target, actionType);           
+                    var reloadUrl = "/cms/mobile/getBoxContent/"+tableIndex;
+                    var tableSelector = "#tableIndex_"+tableIndex;
+                    $(tableSelector).load(reloadUrl);        
                 }
             },
             error: function(e) {
@@ -326,32 +329,6 @@
         });
                
     } 
-
-    function appendDataContainer(url, boxIndex, sectionIndex, value, type, target, actionType)
-    {
-        var obj = '{"url":"' + url + '","sectionIndex":"' +  sectionIndex +
-            '","boxIndex":"' + boxIndex +
-            '","value":"' +value +
-            '","type":"' + type +
-            '","target":"' + target +
-            '","actionType":"' + actionType +'"}';
-        var newBoxIndex = boxIndex + 1;
-        var valueData = "value_"+sectionIndex+"_"+boxIndex;
-        var typeData = "type_"+sectionIndex+"_"+boxIndex;
-        var targetData = "target_"+sectionIndex+"_"+boxIndex;
-        var actionTypeData = "actionType_" + sectionIndex + "_"+ boxIndex;
-
-        var dataToAppend = '<tr id="tr_"'+sectionIndex+ '"><td><div class="btn-toolbar" role="toolbar"><button type="button" class="btn btn-danger edit_btn" id="'+"data_"+sectionIndex+"_"+boxIndex+'"'+
-                                                                    'data="" data-toggle="modal" data-target="#myModal"><span class="glyphicon-center glyphicon glyphicon-cog"></span</button></div></td>'+
-                                '<td id="'+valueData+'">'+value+'</td>' +
-                                '<td id="'+typeData+'">'+type+'</td>' +
-                                '<td id="'+targetData+'">'+target+'</td>' +
-                                '<td id="'+actionTypeData+'">'+actionType+'<span style="display:none;"></span>'+
-                                '<input type="hidden" class="boxContentCount_'+sectionIndex+'" value="'+newBoxIndex+'"/>'
-                                +'</td>'+'</tr>'; 
-        $("#tableme_"+sectionIndex+" tr").last().after(dataToAppend);
-        $("#data_"+ sectionIndex + "_" + boxIndex).attr('data', obj);                                        
-    }       
 
     function setBoxContent(data, url, boxIndex, sectionIndex, value, type, target, actionType, tableIndex) 
     {
