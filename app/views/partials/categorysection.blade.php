fafecf45f233363f79e81fe57cb440f6d8cf64d1
unknown <inon.baguio@yahoo.com>
2015-02-20 16:01:45 +0800

    #492 removed excess markup

--- app/views/partials/categorysection.blade.php
+++ app/views/partials/categorysection.blade.php
@@ -99,62 +99,8 @@
                                                     <div class="col-xs-offset-2 col-xs-10">
                                                         <a1 href="#"  class="btn btn-primary text-center" data-url = "{{{$newHomeCmsLink}}}/addSubCategorySection" id="addSubCategorySection">Add Sub Category</a>
                                                     </div>
-
-                                                </div>
-                                            </form>
-                                            <div class="col-lg-15" style='text-align:center;' id="categorySectionProductPanel_{{$categorySectionIndex}}">
-
-                                                <span style="display:none;">{{$subCategorySectionIndex = 0}}</span>                                                
-                                                <span style="display:none;">{{$categoryProductPanelCount = 1}}</span>                                                
-                                                @foreach($categoryProductPanelList[$categorySectionIndex] as $categorySectionProducts)
-                                                    <div style="position:relative;display:inline-block;">
-                                                        <div class='well' style="height:210px;">
-                                                            <p>
-                                                                <img src="{{$assetLink}}{{ltrim($categorySectionProducts->product_image_path, '.')}}" data-div="" style="width:250px !important;height:150px !important; border: black 1px solid;" class='img-responsive'/>
-                                                            </p>
-                                                            <a href="#categoryProductPanel_{{$categorySectionIndex}}_{{$subCategorySectionIndex}}" data-toggle="modal" style="position:absolute;top:180px;left:135px;"><span class="glyphicon glyphicon-edit" style="font-size:16px;"></span></a>
-                                                            <a class="btn btn-default" 
-                                                                id="removeCategoryProductPanel" 
-                                                                data-index="{{$categorySectionIndex}}" 
-                                                                data-subindex="{{$subCategorySectionIndex}}" 
-                                                                data-nodename="categoryProductPanel" 
-                                                                style="position:absolute;top:2px;left:5px;"
-                                                                data-url = "{{{$newHomeCmsLink}}}/removeContent"
-                                                             ><font color='red'><b>X</b></font></a>
-
-                                                            <a 
-                                                                id="moveupCategoryProductPanel" 
-                                                                data-action="up" 
-                                                                data-index="{{$categorySectionIndex}}" 
-                                                                data-order="{{$subCategorySectionIndex}}" 
-                                                                data-subindex="{{$subCategorySectionIndex}}" 
-                                                                style="position:absolute;top:180px;left:5px;"
-                                                                data-url = "{{{$newHomeCmsLink}}}/setPositionCategoryProductPanel"
-                                                             ><span class="glyphicon glyphicon-chevron-left pull-left" style="font-size:16px;"></span></a>
-
-                                                             <a  
-                                                                id="movedownCategoryProductPanel" 
-                                                                data-action="down" 
-                                                                data-index="{{$categorySectionIndex}}" 
-                                                                data-order="{{$subCategorySectionIndex}}" 
-                                                                data-subindex="{{$subCategorySectionIndex}}" 
-                                                                style="position:absolute;top:180px;right:5px;"
-                                                                data-url = "{{{$newHomeCmsLink}}}/setPositionCategoryProductPanel"
-                                                             ><span class="glyphicon glyphicon-chevron-right pull-right" style="font-size:16px;"></span></a>
-                                                            <span class="categoryProductPanelCount" style="display:none;">{{$categoryProductPanelCount}}</span>
-
-
-
-                                                        <!--Start Edit Slide Modal -->
-                                                            <div class="modal fade user_modal" id="categoryProductPanel_{{$categorySectionIndex}}_{{$subCategorySectionIndex}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-header">
-                                                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                                                                            <h4 class="modal-title white_header" id="myModalLabel"><span class="glyphicon glyphicon-edit"></span>Edit Category Product Panel</h4>
-
                                                 </div>                                      
- 
+                                            </form> 
 
                                             <!-- here -->
 
