94885964fe0cc38ee32b0e4383259139790a0c36
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-04 17:44:32 +0800

    #501 add model for esApiType and esDEviceToken

---
+++ app/models/DeviceToken.php
@@ -0,0 +1,17 @@
+<?php
+
+class DeviceToken extends Eloquent 
+{
+   /**
+    * The database table used by the model.
+    *
+    * @var string
+    */
+    protected $table = 'es_device_token';
+
+   /**
+    * The primary key of the table
+    *
+    */
+    protected $primaryKey = 'id_device_token';
+}
