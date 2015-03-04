7e0ea93eea2b402149a1a97b117fe0bbca27d9a6
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-04 17:45:30 +0800

    #501 add controller for mobile push notification

---
+++ app/controllers/MobileNotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+class MobileNotificationController extends BaseController
+{
+    /**
+     * Show push notification view
+     * @return view
+     */
+    public function showPushNotificationView()
+    {
+        return View::make('pages.mobile-push-notification');
+    }
+
+    public function notifyUser()
+    { 
+        $mobileNotificationService = App::make('MobileNotificationService');
+        $mobileNotificationService->notifyMobileAppUser("HAHAHa", 1);
+    }
+}
