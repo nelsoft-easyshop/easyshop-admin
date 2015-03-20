036fdd7da2660ced4aa236e7134e15ee98589d2b
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-20 14:08:54 +0800

    #501 use square bracket for array

--- app/config/packages/davibennun/laravel-push-notification/config.php
+++ app/config/packages/davibennun/laravel-push-notification/config.php
@@ -1,22 +1,22 @@
 <?php
 
-return array(
-    'IOS_PushNotif_dev'     => array(
+return [
+    'IOS_PushNotif_dev'     => [
         'environment' =>'development',
         'certificate' => app_path().'/certificates/mobile_pushnotification/ckdev.pem',
         'passPhrase'  =>'easyshoppush',
         'service'     =>'apns'
-    ),
-    'IOS_PushNotif_prod'     => array(
+    ],
+    'IOS_PushNotif_prod'     => [
         'environment' =>'production',
         'certificate' => app_path().'/certificates/mobile_pushnotification/ckprod.pem',
         'passPhrase'  =>'easyshoppush',
         'service'     =>'apns'
-    ),
-    'ANDROID_PushNotif' => array(
+    ],
+    'ANDROID_PushNotif' => [
         'environment' =>'environment',
         'apiKey'      =>'AIzaSyBxOFqbxRC5SY5zzKs5zAESfPlliiMaYc0',
         'service'     =>'gcm'
-    )
-);
+    ]
+];
 
