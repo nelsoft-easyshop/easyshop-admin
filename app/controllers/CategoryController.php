846b81e39a4dc648fcf5810e3e4d185d500b3050
Sam <samgavinio@easyshop.ph>
2015-05-22 15:45:00 +0800

    #567 Added missing docs

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -2,6 +2,11 @@
 use Illuminate\Support\Facades\DB;
 class CategoryController extends BaseController
 {
+    /**
+     * Display category list
+     *
+     * @return View
+     */
     public function showAllCategory()
     {
         $userInputCatId = Input::get('id');
@@ -9,10 +14,15 @@ class CategoryController extends BaseController
         $categoryRepository = App::make('CategoryRepository');
 
         return View::make('pages.categorylist')
-            ->with('list_of_category',$categoryRepository->getChildById($catId))
-            ->with('breadcrumbs', $categoryRepository->getParentById($catId));
+                   ->with('list_of_category',$categoryRepository->getChildById($catId))
+                   ->with('breadcrumbs', $categoryRepository->getParentById($catId));
     }
 
+    /**
+     * Updates a category
+     *
+     * @return JSON
+     */
     public function ajaxUpdateCategory()
     {
         $categoryRepository = App::make('CategoryRepository');
@@ -23,7 +33,13 @@ class CategoryController extends BaseController
 
         echo json_encode($category);
     }
+    
 
+    /**
+     * Add a new category
+     *
+     * @return JSON
+     */
     public function ajaxAddCategory()
     {
         $categoryRepository = App::make('CategoryRepository');
@@ -37,14 +53,19 @@ class CategoryController extends BaseController
         echo json_encode($category);
     }
 
+    /**
+     * Searches for a particular category
+     *
+     * @return View
+     */
     public function doSearchCategory()
     {
         $categoryRepository = App::make('CategoryRepository');
         $category = $categoryRepository->search(Input::except('_token'));
 
         return View::make('pages.categorylist')
-            ->with('list_of_category',$categoryRepository->getChildById($category->id_cat))
-            ->with('breadcrumbs', $categoryRepository->getParentById($category->id_cat));
+                   ->with('list_of_category',$categoryRepository->getChildById($category->id_cat))
+                   ->with('breadcrumbs', $categoryRepository->getParentById($category->id_cat));
     }
 
 }
