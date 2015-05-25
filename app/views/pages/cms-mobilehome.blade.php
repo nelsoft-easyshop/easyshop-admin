fa64298ef6602f95994b7a656eb41cbb517d1b8b
Sam <samgavinio@easyshop.ph>
2015-05-25 11:46:09 +0800

    #577 remove unnecessary html elements

--- app/views/pages/cms-mobilehome.blade.php
+++ app/views/pages/cms-mobilehome.blade.php
@@ -25,8 +25,11 @@
                     <ul class="dropdown-menu" role="menu" aria-labelledby="myTabDrop1">
                         <span style="display:none;">{{$index=0}}</span>
                         @foreach($sectionContent as $section)
-                            <li><a href="#page_{{$index}}" tabindex="-1" role="tab" id="sectionNav_{{$index}}" data-toggle="tab">{{{$section->name}}}</a></li>
-
+                            <li>
+                                <a href="#page_{{$index}}" tabindex="-1" role="tab" id="sectionNav_{{$index}}" data-toggle="tab">
+                                    {{{ $categoryLists[(string)$section->name]['name'] }}}
+                                </a>
+                            </li>
                             <span style="display:none;">{{$index++}}</span>
                         @endforeach
                     </ul>
@@ -227,9 +230,13 @@
                                     @foreach($categoryLists as $categories)
                                         @if($categories["name"] !== "PARENT")
                                             @if((string)$categories["slug"] === (string)$section->name)
-                                                <option value="{{{$categories['slug']}}}" data-catname="{{{$categories['name']}}}" selected>{{{$categories["name"]}}} - ({{{$categories['slug']}}})</option>
-                                            @else$categories["slug"]
-                                                <option value="{{{$categories['slug']}}}" data-catname="{{{$categories['name']}}}">{{{$categories["name"]}}} - ({{{$categories['slug']}}})</option>
+                                                <option value="{{{$categories['slug']}}}" data-catname="{{{$categories['name']}}}" selected>
+                                                    {{{$categories["name"]}}} - ({{{$categories['slug']}}})
+                                                </option>
+                                            @else
+                                                <option value="{{{$categories['slug']}}}" data-catname="{{{$categories['name']}}}">
+                                                    {{{$categories["name"]}}} - ({{{$categories['slug']}}})
+                                                </option>
                                             @endif
                                         @endif
                                     @endforeach
@@ -361,15 +368,6 @@
                 <span style="display:none;">{{$index++}}</span>
             @endforeach
 
-            <div class="tab-pane fade" id="profile">
-                <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
-            </div>
-            <div class="tab-pane fade" id="dropdown1">
-                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
-            </div>
-            <div class="tab-pane fade" id="dropdown2">
-                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.</p>
-            </div>
         </div>
 
 
