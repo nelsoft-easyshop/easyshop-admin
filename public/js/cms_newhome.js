04afb17d2ba96b9ee82845027708b6778d36e242
Sam <samgavinio@easyshop.ph>
2015-05-27 17:44:32 +0800

    #586 move sha1 generation to server side: edit other category

--- public/js/cms_newhome.js
+++ public/js/cms_newhome.js
@@ -1914,29 +1914,45 @@
 
     $("#editOtherCategory").on('click','#editOtherCategorySubmit',function (e) { 
         loader.showPleaseWait();           
-        var index = $(this).closest("form").find("#editOtherIndex").val();
-        var url = $(this).closest("form").find("#editOtherUrl").val();
+
+        var $form = $(this).closest("form");
+        var index = $form.find("#editOtherIndex").val();
+        var url = $form.find("#editOtherUrl").val();
         var value = $('#drop_otherCategories_edit option:selected').val();      
-        var hash =  hex_sha1(index + value + userid + password);
-        data = {index:index, value:value, userid:userid, hash:hash, callback:'?'};
 
-        $.ajax({
-            type: 'GET',
-            url: url,
-            data:data,
-            jsonpCallback: 'jsonCallback',
-            contentType: "application/json",
-            dataType: 'jsonp',
-            success: function(json) {
-                loader.hidePleaseWait();   
-                $("#otherCategoriesTable").load("getOtherCategories");
-            },
-            error: function(e) {
-                loader.hidePleaseWait();
-            }
-        });          
-
+        var requestData = {
+            index:index,
+            value:value,            
+            userid:userid
+        };
         
+        $.ajax({
+            url: "/hasher",
+            data: requestData,
+            dataType:"JSON",
+        }).success(function(hash) {             
+            requestData.hash = hash;
+            requestData.callback = '?';
+
+            $.ajax({
+                type: 'GET',
+                url: url,
+                data:requestData,
+                jsonpCallback: 'jsonCallback',
+                contentType: "application/json",
+                dataType: 'jsonp',
+                success: function(json) {
+                    loader.hidePleaseWait();   
+                    $("#otherCategoriesTable").load("getOtherCategories");
+                },
+                error: function(e) {
+                    loader.hidePleaseWait();
+                }
+            });          
+
+        });
+        
+
     });  
 
     $("#editBrandsModal").on('click','#editBrandsSubmit',function (e) { 
