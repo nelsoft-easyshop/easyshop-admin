5b6fd22fb51d6b30462a30fdddd3ad29fe4bbd1d
Sam <samgavinio@easyshop.ph>
2015-05-27 17:15:04 +0800

    #586 move sha1 generation to server side: remove brand

--- public/js/cms_newhome.js
+++ public/js/cms_newhome.js
@@ -1599,34 +1599,47 @@
         }     
     }); 
 
-    $("#manageBrands").on('click','.removeBrands',function (e) { 
-     
-        var index = $(this).data("index").toString();
-        var nodename = $(this).data("nodename");
-        var url = $(this).data("url");
-
-        var hash =  hex_sha1(index + nodename + userid + password);
-        data = { index: index, nodename:nodename, userid:userid, hash:hash, callback:'?'};
-
+    $("#manageBrands").on('click','.removeBrands',function (e) {      
+        var $this = $(this);
+        var index = $this.data("index").toString();
+        var nodename = $this.data("nodename");
+        var url = $this.data("url");       
         var flag = 0;
         var count = parseInt($(".brandsCount").last().text());
         if(count > 1) {
             loader.showPleaseWait();     
+                        
+            var requestData = {
+                index:index,
+                nodename:nodename,               
+                userid:userid                
+            };
+
             $.ajax({
-                type: 'GET',
-                url: url,
-                data:data,
-                jsonpCallback: 'jsonCallback',
-                contentType: "application/json",
-                dataType: 'jsonp',
-                success: function(json) {
-                    loader.hidePleaseWait();   
-                    $("#addBrandsTable").load("getBrandsSection");
-                },
-                error: function(e) {
-                    loader.hidePleaseWait();
-                }
-            });    
+                url: "/hasher",
+                data: requestData,
+                dataType:"JSON",
+            }).success(function(hash) {
+                requestData.hash = hash;
+                requestData.callback = '?';
+                $.ajax({
+                    type: 'GET',
+                    url: url,
+                    data:requestData,
+                    jsonpCallback: 'jsonCallback',
+                    contentType: "application/json",
+                    dataType: 'jsonp',
+                    success: function(json) {
+                        loader.hidePleaseWait();   
+                        $("#addBrandsTable").load("getBrandsSection");
+                    },
+                    error: function(e) {
+                        loader.hidePleaseWait();
+                    }
+                });    
+
+            });
+
         }     
     }); 
 
